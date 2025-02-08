26c5b87e5a7fd2f5f747a47c4c3aeb56ce00303e
HeliceFont <camacho485@gmail.com>
2025-02-08 21:42:19 +0100

    Roc-Job

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Header } from './components/Header'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    
+      <Header></Header>
+      {/* <HeaderBanner></HeaderBanner> */}
+      {/* <Content></Content> */}
+      {/* <Footer></Footer> */}
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import logo from "../../public/image/Recurso11.png"
+
+export const Header = () => {
+  return (
+    <div>
+        <img src={logo} alt="logo" />
+    </div>
+  )
+}
