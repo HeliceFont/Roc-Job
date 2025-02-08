a1bd8c257e4cdec2df77db34bfa34ed78e04893a
HeliceFont <camacho485@gmail.com>
2025-02-08 22:00:45 +0100

    styles
    cambios de estilos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,18 @@
 import logo from "../../public/image/Recurso11.png"
 
 export const Header = () => {
-  return (
-    <div>
-        <img src={logo} alt="logo" />
-    </div>
-  )
+    return (
+        <div>
+            <nav>
+                <img src={logo} alt="logo" />
+                <ul>
+                    <li>Inicio</li>
+                    <li>nosotros</li>
+                    <li>¿Qué hacemos</li>
+                    <li>Cuenta con Nosotros</li>
+                    <li>Contacto</li>
+                </ul>
+            </nav>
+        </div>
+    )
 }
