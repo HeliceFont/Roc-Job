05dd270b56edb8610865aeac45faf9e2c82b9d2b
HeliceFont <camacho485@gmail.com>
2025-02-08 23:07:47 +0100

    Cambio path image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,21 @@
-import logo from "../../public/image/Recurso11.png"
+import logo from "../assets/image/Recurso11.png"
 
 export const Header = () => {
     return (
         <div>
-            <nav>
-                <img src={logo} alt="logo" />
-                <ul>
-                    <li>Inicio</li>
-                    <li>nosotros</li>
-                    <li>¿Qué hacemos</li>
-                    <li>Cuenta con Nosotros</li>
-                    <li>Contacto</li>
-                </ul>
-            </nav>
+            <header>
+                
+                    <img src={logo} alt="logo" />
+                    <nav>
+                    <ul>
+                        <li>Inicio</li>
+                        <li>nosotros</li>
+                        <li>¿Qué hacemos</li>
+                        <li>Cuenta con Nosotros</li>
+                        <li>Contacto</li>
+                    </ul>
+                </nav>
+            </header>
         </div>
     )
 }
