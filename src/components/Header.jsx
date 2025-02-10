760e5384b34d129e1d06039a07c51c6d9bd99913
HeliceFont <camacho485@gmail.com>
2025-02-10 02:18:14 +0100

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import logo from "../assets/image/Recurso11.png"
 import './header.css'
+import iconAccess from '../assets/image/AccessIcon.png'
 
 export const Header = () => {
     return (
@@ -13,6 +14,10 @@ export const Header = () => {
                         <li className="nav-item">¿Qué hacemos?</li>
                         <li className="nav-item">Cuenta con Nosotros</li>
                         <li className="nav-item">Contacto</li>
+                    </ul>
+                    <ul className="nav-list">
+                        <li className="nav-item">Login</li>
+                        <li className="nav-item" id="access">Acceder <img src={iconAccess} alt="" /> </li>
                     </ul>
                 </nav>
             </header>
